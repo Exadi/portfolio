347dd8ff1986ce7d5ae7e291f94a0d8ae4a90c76
Exadi <hallwaywalker@gmail.com>
2023-05-22 23:47:08 -0300

    add contact form to footer

--- src/themes/default/Footer.jsx
+++ src/themes/default/Footer.jsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 import { useSelector } from "react-redux";
 import { findOption } from "../../utils/options";
 import { optionNames } from "./themeOptions";
@@ -6,11 +7,82 @@ import { FaGithub } from "react-icons/fa";
 
 function Footer() {
   const options = useSelector((state) => state.content.options);
+  const contactFormHandler = findOption(options, optionNames.CONTACT_EMAIL);
   const gitHubURL = findOption(options, optionNames.GITHUB_URL);
+
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+
+  const handleChange = (event) => {
+    setFormData({
+      ...formData,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  const contactSubmit = () => {
+    axios.post(contactFormHandler, {
+      name: formData.name,
+      email: formData.email,
+      message: formData.message,
+    });
+  };
+
   return (
     <footer className="footer">
       <div className="wrapper">
-        <h1>Footer</h1>
+        <div className="contact-form">
+          <h2>Contact Me</h2>
+          {
+            /*it should be noted that if the name of the repo isn't exactly "portfolio", the link won't work*/
+            gitHubURL && gitHubURL.value !== "" ? (
+              <p>
+                Please use{" "}
+                <a href={`${gitHubURL.value}/portfolio/issues`}>
+                  GitHub issues
+                </a>{" "}
+                for any feedback related to the site itself. For any other
+                reason, use the form below.
+              </p>
+            ) : null
+          }
+          <form
+            onSubmit={(e) => {
+              e.preventDefault();
+              contactSubmit();
+            }}
+          >
+            <input
+              type="text"
+              name="name"
+              placeholder="Name"
+              value={formData.name}
+              onChange={handleChange}
+            />
+            <input
+              type="text"
+              name="email"
+              placeholder="Email"
+              value={formData.email}
+              onChange={handleChange}
+            />
+            <textarea
+              name="message"
+              placeholder="Message"
+              value={formData.message}
+              onChange={handleChange}
+            />
+            <input
+              type="submit"
+              className="linkButton"
+              style={{ margin: "0.1rem auto" }}
+            ></input>
+          </form>
+        </div>
+
         <div className="social-links">
           {gitHubURL && gitHubURL.value !== "" ? (
             <a href={gitHubURL.value}>
--- src/themes/default/themeOptions.js
+++ src/themes/default/themeOptions.js
@@ -1,7 +1,12 @@
 //a list of options this theme makes use of that are non-standard.
 //these will be added to the settings page if they are not yet saved to the database
-export const optionNames = { GITHUB_URL: "GitHub URL", ABOUT_ME: "About Me" };
+export const optionNames = {
+  CONTACT_EMAIL: "Contact Email Form Handler",
+  GITHUB_URL: "GitHub URL",
+  ABOUT_ME: "About Me",
+};
 const themeOptions = [
+  { name: optionNames.CONTACT_EMAIL, value: "", type: "text", hidden: true },
   { name: optionNames.GITHUB_URL, value: "", type: "text" },
   { name: optionNames.ABOUT_ME, value: "", type: "rte" },
 ];
